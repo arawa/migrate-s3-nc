ce2b04fe1d2518ed4e30a7800196be55ee17440e
zak39 <baptiste.fotia@arawa.fr>
2021-07-28 17:34:42 +0200

    feature(): Get all files foreach users

--- main.php
+++ main.php
@@ -12,8 +12,36 @@ $dotenv->required('NEXTCLOUD_CONFIG_PATH')->notEmpty();
 
 require $_ENV['NEXTCLOUD_FOLDER_PATH'] . $_ENV['NEXTCLOUD_CONFIG_PATH'];
 
+function getFilesFromDir($DIR, &$files = []) {
+
+    $DIRS = array_diff(scandir($DIR), ['.', '..']);
+
+    $DIRS = array_map(function($file) use ($DIR) {
+        return $DIR . '/' . $file;
+    }, $DIRS);
+
+    foreach($DIRS as $file) {
+        if (is_dir($file)) {
+            getFilesFromDir($file, $files);
+        } else {
+            $files[] = $file;
+        }
+    }
+    return $files;
+    
+}
+
 $NEXTCLOUD_VARIABLES_CONFIG = get_defined_vars();
 
 $CONFIG_NEXTCLOUD = $NEXTCLOUD_VARIABLES_CONFIG['CONFIG'];
 
 $DATADIRECTORY = $CONFIG_NEXTCLOUD['datadirectory'];
+
+$usersFolders = array_diff(scandir($DATADIRECTORY), ['.', '..', '.htaccess', '.ocdata', 'nextcloud.log', 'index.html']);
+
+$files = [];
+
+foreach($usersFolders as $userFolder) {
+    $files[$userFolder] = getFilesFromDir($DATADIRECTORY . '/' . $userFolder);
+
+}
