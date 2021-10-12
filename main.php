f25866f7dfc8000ec241dd3a7faa92665a6eb212
zak39 <baptiste.fotia@arawa.fr>
2021-10-12 17:02:18 +0200

    fix: Put an object when we looping filescacheOfOwner immediately afterwards

--- main.php
+++ main.php
@@ -38,64 +38,6 @@ foreach($storages as $storage) {
     $storage->id = $idExplode[1];
 }
 
-// Create jsons folder which contents all json files.
-if (!file_exists(__DIR__ . '/jsons')) {
-    mkdir(__DIR__ . '/jsons');
-}
-
-/** Create json files with the name is the uid/owner of user.
- *  Fill each files an object with these keys : 
- *      - owner (uid/owner of user)
- *      - new_storage (the new storage name for database)
- *      - files (informations on files)
- *          - old_path (current path file)
- *          - new_path (the new file name)
-*/
-foreach($storages as $storage) {
-    $filesByOwner = [];
-	$filesByOwner['owner'] = $storage->id;
-	$filesByOwner['new_storage'] = 'object::user:' . $storage->id;
-	$filesByOwner['files'] = [];
-    $filescacheOfOwner = $db->getFilesCacheByOwner($storage->numeric_id, $directoryUnix->id, $localStorage->numeric_id);
-    foreach($filescacheOfOwner as $filecache) {
-		/** @todo putObject here ? */
-		$dirname = dirname($DATADIRECTORY . '/' . $storage->id . '/' . $filecache->path);
-        $filesByOwner['files'][] = [
-            "old_path" => $DATADIRECTORY . '/' . $storage->id . '/' . $filecache->path,
-            "new_path" => $dirname . '/urn:oid:' . $filecache->fileid,
-            "old_basename" => basename($DATADIRECTORY . '/' . $storage->id . '/' . $filecache->path),
-            "new_basename" => basename($dirname . '/urn:oid:' . $filecache->fileid)
-        ];
-    }
-	/** @todo update table here ? **/
-    file_put_contents(__DIR__ . "/jsons/$storage->id.json", json_encode($filesByOwner, JSON_PRETTY_PRINT));
-    unset($filesByOwner);
-	unset($filescacheOfOwner);
-}
-
-// Get all json files.
-$jsonFiles = array_filter(scandir(__DIR__ . '/jsons'), function($file) {
-    if ($file !== '.' && $file !== '..') {
-        return $file;
-    }
-});
-
-$jsonFiles = array_map(function ($file) {
-    return __DIR__ . "/jsons/" . $file;
-}, $jsonFiles);
-
-// loop all json files to process the migration and update the database.
-
-// {
-//     'path_file' =>
-//     string(77) "/data/nextcloud/nc-s3.dev.arawa.fr/bfotia/files/Documents/Chanson mariage.odt"
-//     'fileid' =>
-//     string(4) "4455"
-//   }
-// {
-/**
- * @todo To search how rename the file at the same time as push
- */
 $s3 = new S3Client([
     'version'   => '2006-03-01',
     'region'    => $_ENV['S3_REGION'],
@@ -106,13 +48,20 @@ $s3 = new S3Client([
     'endpoint'  => $_ENV['S3_ENDPOINT'],
 ]);
 
-foreach ($jsonFiles as $jsonFile) {
-    $json = json_decode(file_get_contents($jsonFile), true);
-    foreach( $json['files'] as $file) {
-        $s3->putObject([
+/** Put for each files on a Object Storage server and
+ * update the database table.
+*/
+foreach($storages as $storage) {
+    $filescacheOfOwner = $db->getFilesCacheByOwner($storage->numeric_id, $directoryUnix->id, $localStorage->numeric_id);
+	$newStorage = 'object::user:' . $storage->id;
+    foreach($filescacheOfOwner as $filecache) {
+		/** @todo putObject here ? */
+		$dirname = dirname($DATADIRECTORY . '/' . $storage->id . '/' . $filecache->path);
+		$s3->putObject([
             'Bucket' => $_ENV['S3_BUCKET_NAME'],
-            'Key'   => $file['new_basename'],
-            'SourceFile'    => $file['base_path'],
+            'Key'   => basename($dirname . '/urn:oid:' . $filecache->fileid),
+            'SourceFile'    => $DATADIRECTORY . '/' . $storage->id . '/' . $filecache->path,
         ]);
     }
+	/** @todo update table here ? **/
 }
