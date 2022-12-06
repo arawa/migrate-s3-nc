8ab87301570d4168746995d09739ddbd412cd6e9
zak39 <baptiste.fotia@arawa.fr>
2022-12-06 16:14:11 +0100

    refactor(php): Add a zero if the month is less than 10
    I added a zero if the month is less than 10 for the file name of the log file.

--- lib/functions.php
+++ lib/functions.php
@@ -4,7 +4,13 @@ function formatDate() {
     $date = getdate();
 
     $formatDate = strval($date['year']);
-    $formatDate .= '-' . strval($date['mon']);
+
+    if ($date['mon'] < 10 ) {
+        $formatDate .= '-' . '0' . strval($date['mon']);
+    } else {
+        $formatDate .= '-' . strval($date['mon']);
+    }
+    
     $formatDate .= '-' . strval($date['mday']);
     $formatDate .= '.' . strval($date['hours']);
     $formatDate .= ':' . strval($date['minutes']);
