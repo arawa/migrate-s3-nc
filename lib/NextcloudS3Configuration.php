e54c7f76bdd3edd580929d7e98254952a5a88185
zak39 <baptiste.fotia@arawa.fr>
2022-12-06 11:44:42 +0100

    refactor(): Generate new S3 Config and config file
    I refactored the generation of the new config Nextcloud with the S3.
    One more, I refactored the generation of the config file for Nextcloud with the S3.

---
+++ lib/NextcloudS3Configuration.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace NextcloudS3Configuration;
+
+use Environment\Environment;
+use NextcloudConfiguration\NextcloudConfiguration;
+
+class NextcloudS3Configuration
+{
+    public static function getS3Configuration(): array
+    {
+        $config = NextcloudConfiguration::getInstance()->getConfig();
+        
+        if (in_array(strtolower($_ENV['S3_PROVIDER_NAME']), Environment::getProvidersS3Swift())) {
+            $config = array_merge($config, self::getS3SwitfConfig());
+        } else {
+            $config = array_merge($config, self::getS3CompatibleConfig());
+        }
+
+        return $config;
+    }
+
+    private function getS3CompatibleConfig(): array
+    {
+        return [
+            'objectstore' =>
+            [
+                'class' => '\\OC\\Files\\ObjectStore\\S3',
+                'arguments' => [
+                    'bucket' => $_ENV['S3_BUCKET_NAME'],
+                    'autocreate'    => true,
+                    'key'   => $_ENV['S3_KEY'],
+                    'secret'    => $_ENV['S3_SECRET'],
+                    'hostname'  => $_ENV['S3_HOSTNAME'],
+                    'port'  => intval($_ENV['S3_PORT']),
+                    'use_ssl'   => true,
+                    'region'    => strtolower($_ENV['S3_REGION']),
+                    'use_path_style' => true
+                ]
+            ]
+        ];
+    }
+
+    private function getS3SwitfConfig(): array
+    {
+        return [
+            'objecstore' => [
+                'class' => 'OC\\Files\\ObjectStore\\Swift',
+                'arguments' => [
+                    'autocreate'    => true,
+                    'user' => [
+                        'name' => $_ENV['S3_SWIFT_USERNAME'],
+                        'password' => $_ENV['S3_SWIFT_PASSWORD'],
+                        'domain' => [
+                            'name'  => 'default'
+                        ],
+                    ],
+                    'scope' => [
+                        'project' => [
+                            'name' => $_ENV['S3_SWIFT_ID_PROJECT'],
+                            'domain' => [
+                                'name' => 'default',
+                            ],
+                        ],
+                    ],
+                    'serviceName' => 'swift',
+                    'region'    => strtoupper($_ENV['S3_REGION']),
+                    'url'   =>  $_ENV['S3_SWIFT_URL'],
+                    'bucket' => $_ENV['S3_BUCKET_NAME'],
+                ]
+            ]
+        ];
+    }
+}
